1b8872326c65d2f2dfabac92e3a3b71ffef506b0
unknown <gustavopf9@gmail.com>
2024-04-26 13:09:58 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function comprado3(){
+    alert("Você comprou o ingresso pro jogo do Corinthians x Fluminense")
+}
+
+function comprado2(){
+    alert("Você comprou o ingresso pro amistoso entre Brasil x Tunínia")
+}
+
+function comprado1(){
+    alert("Você comprou ingresso para a final da libertadores")
+}
+
+
+
+
